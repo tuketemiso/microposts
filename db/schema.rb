e1e5c597874cea256ae1f1e50b981af5f8a94fb7
taichiro kiriyama <taichiro1217@yahoo.co.jp>
2016-10-03 15:09:02 +0000

    add profile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160929142406) do
+ActiveRecord::Schema.define(version: 20161003140432) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160929142406) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "profile"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
