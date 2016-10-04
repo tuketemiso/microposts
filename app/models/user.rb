515d5767702ce8e4af6a150de7dc347d7772779e
taichiro kiriyama <taichiro1217@yahoo.co.jp>
2016-10-04 11:23:30 +0000

    create model micropost

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ActiveRecord::Base
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
+  validates :profile, presence: true, length: { maximum: 200 }
 end
